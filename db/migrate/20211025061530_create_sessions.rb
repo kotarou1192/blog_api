05709afb42854b958dcf52a34d59a88507193654
kousuke_ida <takashiii1192@gmail.com>
2021-10-25 21:11:55 +0900

    add: rspec

--- db/migrate/20211025061530_create_sessions.rb
+++
@@ -1,9 +0,0 @@
-class CreateSessions < ActiveRecord::Migration[6.0]
-  def change
-    create_table :sessions do |t|
-      t.references :user
-      t.string :session_id
-      t.timestamps
-    end
-  end
-end
