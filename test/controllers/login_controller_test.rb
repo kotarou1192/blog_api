6397512311f0751d9509482c06fbb8e93a3a229e
kousuke_ida <takashiii1192@gmail.com>
2021-10-26 14:38:07 +0900

    add: more tests

--- test/controllers/login_controller_test.rb
+++ test/controllers/login_controller_test.rb
@@ -14,4 +14,26 @@ class LoginControllerTest < ActionDispatch::IntegrationTest
     post '/users/login', params: { value: { email: valid_email, password: valid_password } }
     assert @response.status == 200
   end
+
+  test 'invalid password should be rejected' do
+    valid_email = @user.email
+    invalid_password = 'password' << 'invalid'
+    post '/users/login', params: { value: { email: valid_email, password: invalid_password } }
+    assert @response.status == 400
+  end
+
+  test 'invalid email should be rejected' do
+    invalid_email = @user.email + '.invalid'
+    valid_password = 'password'
+    post '/users/login', params: { value: { email: invalid_email, password: valid_password } }
+    assert @response.status == 400
+  end
+
+  test 'invalid email and password pair should be rejected' do
+    invalid_email = @user.email + '.invalid'
+    invalid_password = 'password' << 'invalid'
+
+    post '/users/login', params: { value: { email: invalid_email, password: invalid_password } }
+    assert @response.status == 400
+  end
 end
