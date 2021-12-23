881fa71b0d80befea64ee3cb6df060b590298f79
kousuke_ida <takashiii1192@gmail.com>
2021-12-23 19:13:18 +0900

    update: cors

--- config/application.rb
+++ config/application.rb
@@ -39,17 +39,6 @@ module TodoTree
 
     config.active_job.queue_adapter = :sidekiq
 
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        # TODO: origin
-        hosts_list << Resolv::DNS.new(nameserver: 'ns1.google.com').getresources('o-o.myaddr.l.google.com', Resolv::DNS::Resource::IN::TXT)[0].strings[0]
-        origins 'https://api.takashiii-hq.com', 'https://takashiii-hq.com'
-        resource '*',
-                 headers: :any,
-                 methods: %i[:get, :post, :put, :delete, :options, :head]
-      end
-    end
-
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
