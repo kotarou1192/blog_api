ad113bcff1620fa0863cd1b21a93b11224ab69dc
kousuke_ida <takashiii1192@gmail.com>
2021-12-25 15:34:21 +0900

    add: tests

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,4 +4,32 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = User.find_by(name: 'test_user')
+  end
+
+  test 'blank title should be NoTitle' do
+    post = @user.posts.create(title: '', body: 'body')
+    assert post.title == 'NoTitle'
+  end
+
+  test 'should be valid' do
+    post = @user.posts.create(title: 'title', body: 'body')
+    assert post
+  end
+
+  test 'too long title should be invalid' do
+    post = @user.posts.new(title: 'a' * 256, body: 'body')
+    assert_not post.save
+  end
+
+  test 'should be invalid' do
+    post = @user.posts.new(title: 'title', body: '')
+    assert_not post.save
+  end
+
+  test 'too long body should be invalid' do
+    post = @user.posts.new(title: 'title', body: 'a' * 6001)
+    assert_not post.save
+  end
 end
