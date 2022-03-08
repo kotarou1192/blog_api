57a9d5d8e67014fd2b2d0bb0199216a36394cbef
kousuke ida <takashiii1192@gmail.com>
2022-03-08 14:00:49 +0000

    fix: domains

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,11 @@
 class UserMailer < ApplicationMailer
   PROTOCOL = 'https'
-  DOMAIN = 'takashiii-hq.com'
+  DOMAIN = 'blog-md.net'
+  default from: 'no-reply@blog-md.net'
 
   def account_creation_mail(account_creation_session)
-    @account_creation_url = url_with_params("#{PROTOCOL}://#{DOMAIN}/account/create", session_id: account_creation_session.session_id)
+    @account_creation_url = url_with_params("#{PROTOCOL}://#{DOMAIN}/account/create",
+                                            session_id: account_creation_session.session_id)
     mail to: account_creation_session.email
   end
 end
